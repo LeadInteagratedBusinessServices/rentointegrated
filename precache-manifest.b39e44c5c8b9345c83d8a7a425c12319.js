c69ce5dac67688a8dfab6f9036ed74323984275b
Arivanantham98 <arivanantham1003@gmail.com>
2021-11-03 10:36:53 +0530

    first commit

---
+++ precache-manifest.b39e44c5c8b9345c83d8a7a425c12319.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47015b71d68bccc0d0f13047dcb11de5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b6a01be65f42e023cba",
+    "url": "/static/css/2.8438ca04.chunk.css"
+  },
+  {
+    "revision": "2901fd07e1a566db5afc",
+    "url": "/static/css/main.abb5a8ec.chunk.css"
+  },
+  {
+    "revision": "1b6a01be65f42e023cba",
+    "url": "/static/js/2.16e99e59.chunk.js"
+  },
+  {
+    "revision": "4effa9de81938b2545f40d4f7c3a8025",
+    "url": "/static/js/2.16e99e59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2901fd07e1a566db5afc",
+    "url": "/static/js/main.ebfc4da7.chunk.js"
+  },
+  {
+    "revision": "208783fa02780d630bac",
+    "url": "/static/js/runtime~main.402cc814.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
